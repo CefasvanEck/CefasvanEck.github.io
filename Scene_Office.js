9a951888e0a9ba7607a93ed2a07e9aa1e930c931
CefasvanEck <50441721+CefasvanEck@users.noreply.github.com>
2026-06-02 13:56:48 +0200

    Add files via upload

--- Scene_Office.js
+++ Scene_Office.js
@@ -6,7 +6,10 @@ function anchorDOM(el, x, y)
 class Scene_Office extends Scene 
 {
     button;
+    button_Contact;
     blockTimerUntil;
+    showContactInfo;
+    img_info;
 
     constructor(minTimeWhenShown, maxTimeWhenShown, background_img) 
     {
@@ -15,34 +18,59 @@ class Scene_Office extends Scene
         this.button = createButton("Ik heb van jullie de milieubox ontvangen maar wanneer komen jullie het vieze ophalen?");
         this.button.mousePressed(this.onButtonClick.bind(this));
         this.button.hide();
+
+        this.button_Contact = createButton("Mijn vraag staat er niet tussen.");
+        this.button_Contact.mousePressed(this.onButtonClickContact.bind(this));
+        this.button_Contact.hide();
         this.blockTimerUntil = 0;
+        this.showContactInfo = 0;
+
+        this.img_info = loadImage("Contact_Info.png");
     }
 
     renderFromTimer(currentTime) 
     {
-        super.renderFromTimer(currentTime);
-
-        anchorDOM(this.button, 0.4, 0.43);
-
-        if(currentTime == this.maxTimeWhenRenderThis - 1)
+        if(this.showContactInfo == 1)
         {
-            this.button.remove();
-        }
+            push();
+            imageMode(CENTER);
+            image(this.img_info, Main.widthSize / 2, Main.heightSize / 2, Main.widthSize, Main.heightSize);
+            pop();
 
-        if(currentTime > 315 && this.blockTimerUntil == 0)
-        {
-            textSize(22);
-            noFill();
-            stroke(255);
-            strokeWeight(2);
-            text('Met DRS, waar kan ik u mee helpen?', Main.widthSize * 0.0055, Main.heightSize * 0.5);
-
-            text('Klik op de vraag die u heeft:', Main.widthSize * 0.4, Main.heightSize * 0.4);
-            this.button.show();
             return 2;
         }
-        else return this.blockTimerUntil;
+        else
+        {
+            super.renderFromTimer(currentTime);
+
+            anchorDOM(this.button, 0.4, 0.43);
+            anchorDOM(this.button_Contact, 0.4, 0.46);
+            
+
+            if(currentTime == this.maxTimeWhenRenderThis - 1)
+            {
+                this.button.remove();
+                this.button_Contact.remove();
+            }
+
+            if(currentTime > 315 && this.blockTimerUntil == 0)
+            {
+                textSize(22);
+                noFill();
+                stroke(255);
+                strokeWeight(2);
+                text('Met DRS, waar kan ik u mee helpen?', Main.widthSize * 0.0055, Main.heightSize * 0.5);
+
+                text('Klik op de vraag die u heeft:', Main.widthSize * 0.4, Main.heightSize * 0.4);
+                this.button.show();
+                this.button_Contact.show();
+                return 2;
+            }
+            else return this.blockTimerUntil;
+        }
     }
+
+    
 
     onButtonClick()
     {
@@ -50,5 +78,13 @@ class Scene_Office extends Scene
         console.log("klik werkt");
         Main.pickup_DPD.play();
         sendMail('Watched video until Question in Office Scene with ID: ' + Main.watcherID + ' Question Clicked: ');
+    }
+
+    onButtonClickContact()
+    {
+        this.button.remove();
+        this.button_Contact.remove();
+        this.showContactInfo = 1;
+        sendMail('Has other Question Office Scene with ID: ' + Main.watcherID);
     }
 }
